04b14bec789e4ae4bdffb7d5ee6894ec10714ed5
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-22 22:09:57 +0200

    extensions are done

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -3,8 +3,30 @@ namespace Assignment2.Tests;
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void Test_wordCount_Hello_world_im_clara()
     {
+        //Arrange
+        var input = "Hello world i'm Clara";
+        var expect = 4;
 
+        //Act
+        var actual = Extensions.WordCount(input);
+
+        //Assert
+        Assert.Equal(expect, actual);
+    }
+
+     [Fact]
+    public void Test_uri_scheme_true()
+    {
+        //Arrange
+        var input = new Uri("https://itu.dk/");
+        var expect = true;
+
+        //Act
+        var actual = Extensions.isSecure(input);
+
+        //Assert
+        Assert.Equal(expect, actual);
     }
 }
